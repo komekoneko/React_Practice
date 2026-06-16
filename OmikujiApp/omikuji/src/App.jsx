b54cf8640a3adc01c2d0b382a45d019fee9d525f
komekoneko <kai.s20050701@gmail.com>
2026-06-16 16:37:31 +0900

    add App

--- OmikujiApp/omikuji/src/App.jsx
+++ OmikujiApp/omikuji/src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [lucky, setLucky] = useState("???")
+  const luckies = ["大吉🎉","中吉😊","中吉😊","吉🍀","吉🍀","吉🍀","小吉🌱","小吉🌱","凶💀"]
+
+  const chooseLucky = () => {
+    const resultLucky = Math.floor(Math.random() * luckies.length);
+    setLucky(luckies[resultLucky])
+  }
 
   return (
-    <>
-      <h1>OmikujiApp</h1>
-    </>
+    <div className='app'>
+      <h1>おみくじ</h1>
+      <div className='fuda'></div>
+      <h2>あなたの運勢は{lucky}です</h2>
+      <button className='btn-hiku' onClick={chooseLucky}>ランダムで引く</button>
+    </div>
   )
 }
 
