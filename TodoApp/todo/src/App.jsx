1339fc44c480400c01c0781590f171c573345d63
komekoneko <kai.s20050701@gmail.com>
2026-06-17 13:02:54 +0900

    add Todo

--- TodoApp/todo/src/App.jsx
+++ TodoApp/todo/src/App.jsx
@@ -6,15 +6,31 @@ function App() {
   const [arry, setArry] = useState([]);
 
   const submitBtn = () => {
-
-  }
+    const newArry = [...arry, input];
+    setArry(newArry);
+    setInput("");
+  };
 
   return (
     <>
       <h1>TodoApp</h1>
       <label htmlFor="inp">Todo: </label>
-      <input type="text" value={input} id="inp" placeholder="Todoをここに記入しましょう" onClick={(e) => setInput(e.target.value)}/>
-      <button id="btn" onClick={() => submitBtn}>送信</button>
+      <input
+        type="text"
+        value={input}
+        id="inp"
+        placeholder="Todoをここに記入しましょう"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button id="btn" onClick={submitBtn}>
+        送信
+      </button>
+
+      <ul>
+        {arry.map((arries, index) => {
+          return <li key={index}>{arries}</li>;
+        })}
+      </ul>
     </>
   );
 }
