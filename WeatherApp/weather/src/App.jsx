7f1fe731575a0bc888a3e3fbcbf97ee1bd1f4c17
komekoneko <kai.s20050701@gmail.com>
2026-06-30 12:04:23 +0900

    add littile

--- WeatherApp/weather/src/App.jsx
+++ WeatherApp/weather/src/App.jsx
@@ -6,39 +6,46 @@ function App() {
   const [weather, setWeather] = useState(null);
   const myAPI = "49fcd1c19fd3d85a9b49ddf517ef3df1";
 
-  const searchFn = async(e) => {
-    e.preventDefault()
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myAPI}&units=metric&lang=ja`)
-    const data = await res.json();
-    
-    if(!res.ok){
-      return alert("データが取得できませんでした。")
+  const searchFn = async (e) => {
+    e.preventDefault();
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myAPI}&units=metric&lang=ja`,
+    );
+    if (!res.ok) {
+      return alert("データが取得できませんでした。");
     }
-
+    const data = await res.json();
     setWeather(data);
-
-  }
+  };
 
   return (
     <>
       <form onSubmit={searchFn}>
         <h1>Weather App</h1>
-        <input
-          type="text"
-          value={input}
-          placeholder="国名または都市名を入力"
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button>検索</button>     
-      </form>
-      <li>{weather && (
-        <div>
-          <p>名称：{weather.name}</p>
-          <p>現在の気温：{weather.main.temp}℃</p>
-          <p>現在の空：{weather.weather[0].description}</p>
-          <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+
+        <div className="search-box">
+          <input
+            type="text"
+            value={input}
+            placeholder="国名または都市名を入力"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button>検索</button>
         </div>
-      )}</li>     
+
+        {weather && (
+          <div className="result">
+            <p className="city">{weather.name}</p>
+            <img
+              className="weather-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p className="temp">{Math.round(weather.main.temp)}°</p>
+            <p className="desc">{weather.weather[0].description}</p>
+          </div>
+        )}
+      </form>
     </>
   );
 }
